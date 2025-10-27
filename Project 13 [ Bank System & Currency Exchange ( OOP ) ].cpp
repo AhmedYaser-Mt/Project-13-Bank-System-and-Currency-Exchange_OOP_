5100e18a2992ea52631a172038818e7ade6d679d
AhmedYaser.Mt <ahmed.yaser.mt@gmail.com>
2025-10-27 21:36:19 +0300

    Stage 1 [ Prepare Currency Object ]

--- Project 13 [ Bank System & Currency Exchange ( OOP ) ].cpp
+++ Project 13 [ Bank System & Currency Exchange ( OOP ) ].cpp
@@ -1,17 +1,46 @@
 #include <iostream>
-#include "clsLoginScreen.h"
+#include "clsCurrency.h"
 
 using namespace std;
 
+void PrintCurrency(clsCurrency& Currency)
+{
+    cout << "\nCurrency Card:\n";
+    cout << "_________________________________\n";
+    cout << "\nCountry     : " << Currency.Country();
+    cout << "\nCode        : " << Currency.CurrencyCode();
+    cout << "\nName        : " << Currency.CurrencyName();
+    cout << "\nRate (1$) = : " << Currency.Rate();
+    cout << "\n_______________________________\n";
+}
+
 int main()
 {
-    while (true)
+    clsCurrency Currency1 = clsCurrency::FindByCurrencyCode("AE");
+
+    if (Currency1.IsEmpty())
     {
-        if (!clsLoginScreen::ShowLoginScreen())
-        {
-            break;
-        }
+        cout << "\nCurrency Is Not Found!\n";
     }
+    else
+    {
+        PrintCurrency(Currency1);
+    }
+
+    clsCurrency Currency2 = clsCurrency::FindByCountry("A");
+
+    if (Currency2.IsEmpty())
+    {
+        cout << "\nCurrency Is Not Found!\n";
+    }
+    else
+    {
+        PrintCurrency(Currency2);
+    }
+
+    cout << "\nCurrency 1 After updating Rate :\n";
+    Currency1.UpdateRate(3.7);
+    PrintCurrency(Currency1);
 
     return 0;
 }
